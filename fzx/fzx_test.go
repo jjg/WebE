ea99a810495c03d0ab4260bae69c0111fafc2f4c
Jason J. Gullickson <mr@jasongullickson.com>
2022-03-09 08:39:02 -0600

    Split POST/PUT tests out so we can verify contents.

--- fzx/fzx_test.go
+++ fzx/fzx_test.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/jjg/WebE/fzx/quiet"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestRequestMethods(t *testing.T) {
+func TestReadMethods(t *testing.T) {
 
 	// Mute output while running tests.
 	defer quiet.BeQuiet()()
@@ -19,8 +22,8 @@ func TestRequestMethods(t *testing.T) {
 	}{
 		{http.MethodHead, "http://example.com"},
 		{http.MethodGet, "http://example.com"},
-		{http.MethodPost, "http://example.com"},
-		{http.MethodPut, "http://example.com"},
+		//{http.MethodPost, "http://example.com"},
+		//{http.MethodPut, "http://example.com"},
 		{http.MethodDelete, "http://example.com"},
 		// NOTE: EXECUTE is a made-up method specific to fzx,
 		// maybe we'll create an RFC for it once this takes off...
@@ -37,4 +40,46 @@ func TestRequestMethods(t *testing.T) {
 			t.Fatalf("expected %v, got %v", want, got)
 		}
 	}
+}
+
+func TestPostPut(t *testing.T) {
+
+	testFileContents := "A plain text file to test the POST and PUT methods."
+
+	// Write testFileContents to a file.
+	f, err := os.CreateTemp("", "posttest.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(f.Name()) // clean up
+
+	if _, err := f.Write([]byte(testFileContents)); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	// POST file to /testing/posttest.txt
+	req := httptest.NewRequest(http.MethodPost, "http://localhost:7302/testing/posttest.txt", f)
+	w := httptest.NewRecorder()
+
+	Handler(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+
+	// Make a HEAD request for /testing/posttest.txt and compare header values.
+	req = httptest.NewRequest(http.MethodHead, "http://localhost:7302/testing/posttest.txt", nil)
+
+	Handler(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+
+	// Make a GET request for /testing/posttest.txt and compare the contents.
+	req = httptest.NewRequest(http.MethodGet, "http://localhost:7302/testing/posttest.txt", nil)
+
+	Handler(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
+	assert.Equal(t, testFileContents, w.Body.String())
 }
